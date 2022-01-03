9a49edc8153b0c22b92a140b4f7d39a901aac8f7
izipei <jinsoyun141@gmail.com>
2022-01-03 19:09:44 +0800

    跳跃

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb; // 父组件的刚体组件
 
-    public float speed; // 移速
+    public float mvoeSpeed; // 移速
+    public float jumpForce; // 跳跃力
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private bool wJump = false; // 记录跳跃键状态在下一逻辑帧更新
+
     // Update is called once per frame
     void Update()
+    {
+        // 检测跳跃键
+        if (Input.GetButtonDown("Jump"))
+        {
+            wJump = true;
+        }
+    }
+
+    void FixedUpdate()
     {
         Move();
     }
@@ -37,12 +49,31 @@ public class PlayerController : MonoBehaviour
         }
 
         // 获取输入
-        float horizontalSpeed = speed * Input.GetAxis("Horizontal");
-        float verticalSpeed = rb.velocity.y;
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float faceDirection = Input.GetAxisRaw("Horizontal");
 
         // 设置速度
-        // rb.velocity.Set(horizontalSpeed, verticalSpeed);
+        float horizontalSpeed = horizontalMove * mvoeSpeed * Time.fixedDeltaTime;
+        float verticalSpeed = rb.velocity.y;
+
+        // 根据跳跃键状态设置竖直速度
+        if (wJump)
+        {
+            // 重置wJump
+            wJump = false;
+            verticalSpeed = jumpForce * Time.fixedDeltaTime;
+        }
+
         rb.velocity = new Vector2(horizontalSpeed, verticalSpeed);
+
+        // 设置朝向
+        if (faceDirection != 0)
+        {
+            Vector3 scale = rb.transform.localScale;
+            float scaleX = faceDirection * Mathf.Abs(scale.x);
+
+            rb.transform.localScale = new Vector3(scaleX, scale.y, scale.z);
+        }
     }
 
     // 能否移动
